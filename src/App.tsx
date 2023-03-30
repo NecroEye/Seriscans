da07fbe71b96bb79fa0f979b520a8b0d293c6cf0
NecroEye <murat.can1250@gmail.com>
2023-03-30 23:57:26 +0300

    TabNavigator Added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
-import React from 'react';
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import {NavigationContainer} from "@react-navigation/native";
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import MainMenu from "./Pages/MainMenu";
 import InfoPage from "./Pages/InfoPage";
 import ReadingPage from "./Pages/ReadingPage";
 
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
-function App(){
+const SpecialArea = () => {
 
-  return(
+  return (
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="InfoArea" component={InfoPage} />
+      <Tab.Screen name="ReadingPage" component={ReadingPage} />
+    </Tab.Navigator>
+  );
+};
+
+
+function App() {
+
+  return (
 
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown:false}}>
-        <Stack.Screen name="MainMenu" component={MainMenu}/>
-        <Stack.Screen name="InfoPage" component={InfoPage}/>
-        <Stack.Screen name="ReadingPage" component={ReadingPage}/>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="MainMenu" component={MainMenu} />
+        <Stack.Screen name="InfoPage" component={SpecialArea} />
+        <Stack.Screen name="ReadingPage" component={ReadingPage} />
       </Stack.Navigator>
     </NavigationContainer>
 
